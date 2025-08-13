ec57564e641793fef064b1154b5a0d43527bff7d
luotao <luotaoyeah@gmail.com>
2025-08-13 23:20:37 +0800

    feat(mastering-powershell-scripting): 1.8.1.5 switch parameters

--- projects/mastering-powershell-scripting/src/_01_08_01_05/01.ps1
+++ projects/mastering-powershell-scripting/src/_01_08_01_05/01.ps1
@@ -1,4 +1,8 @@
-# switch parameter 开关参数, 不需要传参数值,
+# 1.8.1.5 switch parameters
+
+# switch parameter, 不需要传参数值, 表示开发参数,
+
+
 # 如果希望动态设置开关参数是否生效, 可以用下面的方式设置,
 
 # 表示开关参数 -Recurse 生效了,
@@ -6,6 +10,3 @@ Get-ChildItem -Recurse:$true
 
 # 表示开关参数 -Recurse 未生效,
 Get-ChildItem -Recurse:$false
-
-# 等待用户输入, 用于阻止当前窗口退出,
-Read-Host
